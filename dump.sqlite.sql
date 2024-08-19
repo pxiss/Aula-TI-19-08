c94fda3ab2ad1ca4162ba47023066e84a8190e7d
pxiss <159054757+pxiss@users.noreply.github.com>
2024-08-19 14:01:45 -0300

    19/08/2024 13:59

---
+++ dump.sqlite.sql
@@ -0,0 +1,17 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE produtos(
+  
+  	id integer primary key AUTOINCREMENT,
+  nome varchar(100) not null,
+  descricao text,
+  preco decimal(10,2),
+  dataHora datetime default CURRENT_TIMESTAMP
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
